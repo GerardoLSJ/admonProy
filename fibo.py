3ceb4f76bec1c57333964b33bfc616d21624de6c
IsraelHernandezG <36481990+IsraelHernandezG@users.noreply.github.com>
2018-02-14 20:45:31 -0600

    Add files via upload
    Se agregaron mensajes para el usuario

--- fibo.py
+++ fibo.py
@@ -2,12 +2,15 @@
 #
 # Team Members:
 # Gonzalez Valdez Bryan
-# Lopez Santibañez Jimenez Luis Gerardo
-# Moreno Guerra Marco Antonio
+# Lopez Santibanez Jimenez Luis Gerardo
+# Hernandez Garcia Israel
 #
 from math import sqrt
+print("Dada la funcion f=(1+5^(1/2))^n-(1-5^(1/2))^n)/(2^n x 5^(1/2))\n")
+print("y numero n, este programa obtiene la f(n) asociada.\n")
 def F(n):
     return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
 
+print("con n=10\nf=")
 print(F(10))
 #Output 55.000000000000014 
